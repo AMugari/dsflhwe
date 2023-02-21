15eabfbf8e682d2e683a86262805a60fb991d17d
apexbravo <abelrevelation@gmail.com>
2023-02-21 07:38:48 +0200

    Just a demo

---
+++ java34/src/main/java/com/mycompany/java34/Java34.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java34;
+
+/**
+ *
+ * @author apexb
+ */
+public class Java34 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
